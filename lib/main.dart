65f3a4570f55c2073ef6fcb0265a410c60c5cf51
natb1899 <natal.bumann@students.hevs.ch>
2022-11-18 18:52:34 +0100

    added the bottom navigation & a few example pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,122 @@
 import 'package:flutter/material.dart';
+import 'pages/routes.dart';
+import 'pages/profile.dart';
+import 'pages/myRoutes.dart';
 
 void main() {
-
-
-
-
-
-
+  runApp(const MyApp());
 }
-//test
-
-
-
-
-
-
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const RootPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RootPage> createState() => _RootPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  final List<Widget> pages = const [Routes(), Profile(), MyRoutes()];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = const Routes();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Text"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: PageStorage(
+        bucket: bucket,
+        child: currentScreen,
+      ),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.orange,
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 10,
+          child: SizedBox(
+            height: 65,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: 40.0,
+                  onPressed: () {
+                    setState(() {
+                      currentScreen = const Routes();
+                      currentPage = 0;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.pedal_bike,
+                          color:
+                              currentPage == 0 ? Colors.white : Colors.black),
+                      Text('All Routes',
+                          style: TextStyle(
+                              color: currentPage == 0
+                                  ? Colors.white
+                                  : Colors.black))
+                    ],
+                  ),
+                ),
+                MaterialButton(
+                  minWidth: 40.0,
+                  onPressed: () {
+                    setState(() {
+                      currentScreen = const Profile();
+                      currentPage = 1;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.pedal_bike,
+                          color:
+                              currentPage == 1 ? Colors.white : Colors.black),
+                      Text('Profile',
+                          style: TextStyle(
+                              color: currentPage == 1
+                                  ? Colors.white
+                                  : Colors.black))
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          )),
+      floatingActionButton: SizedBox(
+        height: 75.0,
+        width: 75.0,
+        child: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              currentScreen = const MyRoutes();
+              currentPage = 2;
+            });
+          },
+          backgroundColor: Colors.orange,
+          tooltip: 'Add',
+          elevation: 0,
+          child: const Icon(Icons.add),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
---
+++ lib/pages/routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Routes extends StatefulWidget {
+  const Routes({super.key});
+
+  @override
+  State<Routes> createState() => _RoutesState();
+}
+
+class _RoutesState extends State<Routes> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('All Routes'),
+      ),
+    );
+  }
+}
