0fc66b0298b579f2cdf8a2705a01efb15d3c9c1b
BenjaminMorel <benjamin.morel@students.hevs.ch>
2022-11-24 13:08:10 +0100

    Modifs map

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -28,59 +28,60 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-
   // Hardcoded markers to test, later use firebase
-  final _markers = [
+  var points = <LatLng>[
     LatLng(46.28294058464128, 7.5387422133790745),
     LatLng(46.29273682028264, 7.5361982764216275),
   ];
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:  FlutterMap(
-          options: MapOptions(
-            center: LatLng(46.28732243981999, 7.535148068628832),
-            zoom: 15.0,
-          ),
-          mapController: MapController(),
-          children: [
-            TileLayer(
-              urlTemplate: "https://api.mapbox.com/styles/v1/glacia/clauxhpdp007715qmh37hhvdn/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZ2xhY2lhIiwiYSI6ImNsYXV4NWNnZDAwODgzeW81ODJkNzlxaWcifQ.GHlRSCMMR-M9BzZg9247Cg",
-              additionalOptions: const {
-                'accessToken': 'pk.eyJ1IjoiZ2xhY2lhIiwiYSI6ImNsYXV4NWNnZDAwODgzeW81ODJkNzlxaWcifQ.GHlRSCMMR-M9BzZg9247Cg',
-                'id': 'mapbox.mapbox-streets-v8'
-              },
-              //userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-              //urlTemplate: 'https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/{z}/{y}/{x}.jpeg',
-            ),
-            MarkerLayer(
-              markers: [
-                Marker(
-                  point: LatLng(46.28294058464128, 7.5387422133790745),
-                  builder: (context) => const Icon(
-                    Icons.location_on,
-                    color: Colors.green,
-                  ),
-                ),
-                Marker(
-                  point: LatLng(46.29273682028264, 7.5361982764216275),
-                  builder: (context) => const Icon(
-                    Icons.location_on,
-                    color: Colors.red,
-                  ),
-                ),
-              ],
-            )
-          ],
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(46.28732243981999, 7.535148068628832),
+          zoom: 15.0,
         ),
-         );
-
-    // new PolylineLayer(
-    //   polylines: [
-    //     new Polyline(points: points, strokeWidth:  5.0, color: Colors.red),
-    //   ],
-    // );
+        mapController: MapController(),
+        children: [
+          TileLayer(
+            urlTemplate:
+                "https://api.mapbox.com/styles/v1/glacia/clauxhpdp007715qmh37hhvdn/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZ2xhY2lhIiwiYSI6ImNsYXV4NWNnZDAwODgzeW81ODJkNzlxaWcifQ.GHlRSCMMR-M9BzZg9247Cg",
+            additionalOptions: const {
+              'accessToken':
+                  'pk.eyJ1IjoiZ2xhY2lhIiwiYSI6ImNsYXV4NWNnZDAwODgzeW81ODJkNzlxaWcifQ.GHlRSCMMR-M9BzZg9247Cg',
+              'id': 'mapbox.mapbox-streets-v8'
+            },
+            //userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            //urlTemplate: 'https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/{z}/{y}/{x}.jpeg',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(46.28294058464128, 7.5387422133790745),
+                builder: (context) => const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ),
+              Marker(
+                point: LatLng(46.29273682028264, 7.5361982764216275),
+                builder: (context) => const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+          PolylineLayer(
+            polylines: [
+              Polyline(points: points, strokeWidth: 5.0, color: Colors.blue),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
