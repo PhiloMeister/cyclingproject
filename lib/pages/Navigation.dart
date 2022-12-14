cb7218a2092451a16e586b11d3a7af6907be7d17
natb1899 <natal.bumann@students.hevs.ch>
2022-12-14 09:42:39 +0100

    small changes

--- lib/pages/Navigation.dart
+++ lib/pages/Navigation.dart
@@ -51,60 +51,46 @@ class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      appBar: AppBar(
-        backgroundColor: const Color(0XFFD9D9D9),
-        title: SizedBox(
-            width: 120,
-            child: Image.asset("assets/images/logo-bikevs-cropped.png")),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Profile()),
-                );
-              },
-              icon: const Icon(Icons.account_circle_rounded))
-        ],
-      ),
-      body: _pages[currentPage],
-      bottomNavigationBar: Container(
-        color: const Color(0XFF1f1f1f),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-          child: GNav(
-              backgroundColor: const Color(0xFF1F1F1F),
-              color: Colors.white,
-              activeColor: const Color(0XFFB61818),
-              tabBackgroundColor: Colors.grey.shade200,
-              tabBorderRadius: 15,
-              gap: 8,
-              onTabChange: (index) {
-                _navigateBottomBar(index);
-              },
-              padding: const EdgeInsets.all(12),
-              tabs: const [
-                GButton(icon: Icons.home, text: 'All routes'),
-                GButton(icon: Icons.map, text: 'Map'),
-                GButton(icon: Icons.heart_broken, text: 'Liked routes'),
-              ]),
+        backgroundColor: Colors.grey.shade200,
+        appBar: AppBar(
+          backgroundColor: const Color(0XFFD9D9D9),
+          title: SizedBox(
+              width: 120,
+              child: Image.asset("assets/images/logo-bikevs-cropped.png")),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Profile()),
+                  );
+                },
+                icon: const Icon(Icons.account_circle_rounded))
+          ],
         ),
-      ),
-      floatingActionButton: SizedBox(
-        child: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              currentPage = 3;
-            });
-          },
-          backgroundColor: const Color(0XFF1f1f1f),
-          tooltip: 'Add',
-          elevation: 0,
-          child: const Icon(Icons.add),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-    );
+        body: _pages[currentPage],
+        bottomNavigationBar: Container(
+          color: const Color(0XFF1f1f1f),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+            child: GNav(
+                backgroundColor: const Color(0xFF1F1F1F),
+                color: Colors.white,
+                activeColor: const Color(0XFFB61818),
+                tabBackgroundColor: Colors.grey.shade200,
+                tabBorderRadius: 15,
+                gap: 8,
+                onTabChange: (index) {
+                  _navigateBottomBar(index);
+                },
+                padding: const EdgeInsets.all(12),
+                tabs: const [
+                  GButton(icon: Icons.home, text: 'All routes'),
+                  GButton(icon: Icons.map, text: 'Map'),
+                  GButton(icon: Icons.heart_broken, text: 'Liked routes'),
+                ]),
+          ),
+        ));
   }
 }
